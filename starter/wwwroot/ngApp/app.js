1e4c32900e21989588e8ee4c264fb96fc8191d00
Jason Tennant <xcodula@icloud.com>
2017-09-01 06:00:31 -0700

    add w3.css, remove bootstrap

--- starter/wwwroot/ngApp/app.js
+++ starter/wwwroot/ngApp/app.js
@@ -1,4 +1,4 @@
-var myApp = angular.module("myApp", ['ui.router', 'ngResource', 'ui.bootstrap']);
+var myApp = angular.module("myApp", [ "ui.router", "ngResource" ]);
 
 myApp.controller("AboutController", AboutController);
 myApp.controller("AccountController", AccountController);
@@ -14,70 +14,62 @@ myApp.service("$accountService", AccountService);
 myApp.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
 
     $stateProvider
-        .state('home', {
-            url: '/',
-            templateUrl: '/ngApp/views/home.html',
+        .state("home", {
+            url: "/",
+            templateUrl: "/ngApp/views/home.html",
             controller: HomeController,
-            controllerAs: 'controller'
-        })
-        .state('secret', {
-            url: '/secret',
-            templateUrl: '/ngApp/views/secret.html',
+            controllerAs: "controller"
+        }).state("secret", {
+            url: "/secret",
+            templateUrl: "/ngApp/views/secret.html",
             controller: SecretController,
-            controllerAs: 'controller'
-        })
-        .state('login', {
-            url: '/login',
-            templateUrl: '/ngApp/views/login.html',
+            controllerAs: "controller"
+        }).state("login", {
+            url: "/login",
+            templateUrl: "/ngApp/views/login.html",
             controller: LoginController,
-            controllerAs: 'controller'
-        })
-        .state('register', {
-            url: '/register',
-            templateUrl: '/ngApp/views/register.html',
+            controllerAs: "controller"
+        }).state("register", {
+            url: "/register",
+            templateUrl: "/ngApp/views/register.html",
             controller: RegisterController,
-            controllerAs: 'controller'
-        })
-        .state('externalRegister', {
-            url: '/externalRegister',
-            templateUrl: '/ngApp/views/externalRegister.html',
+            controllerAs: "controller"
+        }).state("externalRegister", {
+            url: "/externalRegister",
+            templateUrl: "/ngApp/views/externalRegister.html",
             controller: ExternalRegisterController,
-            controllerAs: 'controller'
-        })
-        .state('about', {
-            url: '/about',
-            templateUrl: '/ngApp/views/about.html',
+            controllerAs: "controller"
+        }).state("about", {
+            url: "/about",
+            templateUrl: "/ngApp/views/about.html",
             controller: AboutController,
-            controllerAs: 'controller'
-        })
-        .state('notFound', {
-            url: '/notFound',
-            templateUrl: '/ngApp/views/notFound.html'
+            controllerAs: "controller"
+        }).state("notFound", {
+            url: "/notFound",
+            templateUrl: "/ngApp/views/notFound.html"
         });
 
     // Handle request for non-existent route
-    $urlRouterProvider.otherwise('/notFound');
+    $urlRouterProvider.otherwise("/notFound");
 
     // Enable HTML5 navigation
     $locationProvider.html5Mode(true);
 });
 
-angular.module('myApp')
-       .factory('authInterceptor', ($q, $window, $location) =>
-        ({
-            request: function (config) {
-                config.headers = config.headers || {};
-                config.headers['X-Requested-With'] = 'XMLHttpRequest';
-                return config;
-            },
-            responseError: function (rejection) {
-                if (rejection.status === 401 || rejection.status === 403) {
-                    $location.path('/login');
-                }
-                return $q.reject(rejection);
-            }
-        }));
+myApp.factory("authInterceptor", ($q, $window, $location) => ({
+    request: function (config) {
+        config.headers = config.headers || {};
+        config.headers["X-Requested-With"] = "XMLHttpRequest";
+        return config;
+    },
+    responseError: function (rejection) {
+        if (rejection.status === 401 || rejection.status === 403) {
+            $location.path("/login");
+        }
+        return $q.reject(rejection);
+    }
+}));
 
-angular.module('myApp').config(function ($httpProvider) {
-    $httpProvider.interceptors.push('authInterceptor');
+myApp.config(function ($httpProvider) {
+    $httpProvider.interceptors.push("authInterceptor");
 });
--- training/wwwroot/ngApp/app.js
+++ training/wwwroot/ngApp/app.js
@@ -1,18 +1,18 @@
-var myApp = angular.module("myApp", ['ui.router', 'ngResource', 'ui.bootstrap']);
+var myApp = angular.module("myApp", [ "ui.router", "ngResource" ]);
 myApp.controller("MainController", MainController);
 myApp.service("$productService", ProductService);
 
 myApp.config(function ($stateProvider, $urlRouterProvider, $locationProvider) {
     $stateProvider
-        .state('home', {
-            url: '/',
-            templateUrl: '/ngApp/views/main.html',
+        .state("home", {
+            url: "/",
+            templateUrl: "/ngApp/views/main.html",
             controller: MainController,
-            controllerAs: 'controller'
+            controllerAs: "controller"
         });
 
     // Handle request for non-existent route
-    $urlRouterProvider.otherwise('/notFound');
+    $urlRouterProvider.otherwise("/notFound");
 
     // Enable HTML5 navigation
     $locationProvider.html5Mode(true);
